8ee1bac2f8980127e29442e4a7d07e5122524740
Jani Levonen <jani.levonen@sitowise.com>
2019-10-15 11:54:50 +0300

    Revert earlier changes to thematic map layer ID by request from the client

--- bundles/mapping/mapstats/plugin/StatsLayerPlugin.js
+++ bundles/mapping/mapstats/plugin/StatsLayerPlugin.js
@@ -45,7 +45,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapstats.plugin.StatsLayerPlugin
             if (config.useArcGis) {
                 me.useArcGis = config.useArcGis;
                 me.featureAttribute = 'KuntaNro';
-                me.defaultLayerId = '25';
+                me.defaultLayerId = '21';
             }                
         }
     }, {
