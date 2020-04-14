02dff215a4d3ef5dc2d26bde32b67178fc9afed9
Krzysztof Kozlowski <krzysztof.kozlowski@sitopoland.com>
2020-04-14 11:57:35 +0200

    show empty values in feature properties

--- bundles/liiteri/liiteri-statsgrid/plugin/ManageStatsPlugin.js
+++ bundles/liiteri/liiteri-statsgrid/plugin/ManageStatsPlugin.js
@@ -6866,7 +6866,7 @@ Oskari.clazz.define('Oskari.statistics.bundle.statsgrid.plugin.ManageStatsPlugin
 
                 for (var propName in headerRow) {
                     if (propName != "municipality" && propName != "code") {
-                        preparedFeature.properties["#STAT_ATTRIBUTE#" + headerRow[propName]] = foundDataRow ? foundDataRow[propName] : '';
+                        preparedFeature.properties["#STAT_ATTRIBUTE#" + headerRow[propName]] = (foundDataRow && foundDataRow[propName]) ? foundDataRow[propName] : '';
                     }
                 }
 
